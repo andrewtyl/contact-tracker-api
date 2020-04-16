7859a9350dfa0b219ccb386c9dc9efba187ba02e
Andrew Jessen-Tyler <andrewtyl.pro@gmail.com>
2020-04-16 13:32:59 -0600

    prepped for travis. Plus some updates to packages and npm v

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
-  NODE_ENV: process.env.NODE_ENV || 'development',
+  NODE_ENV: process.env.NODE_ENV || 'production',
   KNEX_CON: `postgres://${process.env.PG_USER}:${process.env.PG_PASS}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DB}`
 }
